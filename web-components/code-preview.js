cc70ceab7e91183e5bfe7e21bf37b07605d95ddc
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-02-28 21:41:44 +0530

    Editor fixes

--- web-components/code-preview.js
+++ web-components/code-preview.js
@@ -1,31 +1,136 @@
 function dragElement(element) {
-    // Initialize variables for tracking drag state
-    let isDragging = false;
-    let offsetX, offsetY;
-
     // Reference to the entire window
     const windowElement = element;
+    const title= element.querySelector('.header');
+    const cornerRB= element.querySelector('.corner-rb')
+    const cornerLB= element.querySelector('.corner-lb')
+    const cornerL = element.querySelector('.corner-l');
+    const cornerR= element.querySelector('.corner-r')
+    const cornerB= element.querySelector('.corner-b')
 
-    // Event listeners for starting and stopping the drag
-    windowElement.addEventListener('mousedown', (e) => {
-      isDragging = true;
-      offsetX = e.clientX - windowElement.getBoundingClientRect().left;
-      offsetY = e.clientY - windowElement.getBoundingClientRect().top;
+    title.addEventListener('mousedown', (e) => {
+        title.style.background = '#202020';
+        // Initialize variables for tracking drag state
+        let offsetX = e.clientX - windowElement.getBoundingClientRect().left;
+        let offsetY = e.clientY - windowElement.getBoundingClientRect().top;
+
+        const drag = (e) => {
+            e.preventDefault()
+
+            const newX = e.clientX - offsetX;
+            const newY = e.clientY - offsetY;
+
+            windowElement.style.left = `${newX}px`;
+            windowElement.style.top = `${newY}px`;
+        }
+        const mouseup = () => {
+            title.style.background = 'black';
+
+            document.removeEventListener('mousemove', drag);
+            document.removeEventListener('mouseup', mouseup);
+        }
+
+        document.addEventListener('mouseup', mouseup);
+        document.addEventListener('mousemove', drag);
+
     });
+    cornerRB.addEventListener('mousedown', (e) => {
+        let offsetX = e.clientX - windowElement.clientWidth;
+        let offsetY = e.clientY - windowElement.clientHeight;
 
-    window.addEventListener('mouseup', () => {
-      isDragging = false;
+        const drag = (e) => {
+            e.preventDefault()
+
+            const newX = e.clientX - offsetX;
+            const newY = e.clientY - offsetY;
+
+            windowElement.style.width = `${newX}px`;
+            windowElement.style.height = `${newY}px`;
+        }
+        const mouseup = () => {
+            document.removeEventListener('mousemove', drag);
+            document.removeEventListener('mouseup', mouseup);
+        }
+
+        document.addEventListener('mouseup', mouseup);
+        document.addEventListener('mousemove', drag);
+
     });
+    cornerLB.addEventListener('mousedown', (e) => {
+        let prevX = e.clientX;
+        let prevY = e.clientY;
 
-    // Event listener for dragging the window
-    window.addEventListener('mousemove', (e) => {
-      if (isDragging) {
-        const newX = e.clientX - offsetX;
-        const newY = e.clientY - offsetY;
+        const drag = (e) => {
+            let rect = windowElement.getBoundingClientRect();
+            e.preventDefault()
 
-        windowElement.style.left = `${newX}px`;
-        windowElement.style.top = `${newY}px`;
-      }
+            windowElement.style.width = rect.width + (prevX - e.clientX) + "px";
+            windowElement.style.height = rect.height - (prevY - e.clientY) + "px";
+            windowElement.style.left = rect.left - (prevX - e.clientX) + "px";
+
+            prevX = e.clientX;
+            prevY = e.clientY;
+        }
+        const mouseup = () => {
+            document.removeEventListener('mousemove', drag);
+            document.removeEventListener('mouseup', mouseup);
+        }
+
+        document.addEventListener('mouseup', mouseup);
+        document.addEventListener('mousemove', drag);
+
+    });
+    cornerL.addEventListener('mousedown', (e) => {
+        let prevX = e.clientX;
+        const drag = (e) => {
+            let rect = windowElement.getBoundingClientRect();
+            e.preventDefault()
+
+            windowElement.style.width = rect.width + (prevX - e.clientX) + "px";
+            windowElement.style.left = rect.left - (prevX - e.clientX) + "px";
+
+            prevX = e.clientX;
+        }
+        const mouseup = () => {
+            document.removeEventListener('mousemove', drag);
+            document.removeEventListener('mouseup', mouseup);
+        }
+
+        document.addEventListener('mouseup', mouseup);
+        document.addEventListener('mousemove', drag);
+
+    });
+    cornerR.addEventListener('mousedown', (e) => {
+        let offsetX = e.clientX - windowElement.clientWidth;
+        const drag = (e) => {
+            e.preventDefault();
+            const newX = e.clientX - offsetX;
+            windowElement.style.width = `${newX}px`;
+        }
+        const mouseup = () => {
+            document.removeEventListener('mousemove', drag);
+            document.removeEventListener('mouseup', mouseup);
+        }
+
+        document.addEventListener('mouseup', mouseup);
+        document.addEventListener('mousemove', drag);
+
+    });
+    cornerB.addEventListener('mousedown', (e) => {
+        let offsetY = e.clientY - windowElement.clientHeight;
+        const drag = (e) => {
+            e.preventDefault()
+            const newY = e.clientY - offsetY;
+            windowElement.style.height = `${newY}px`;
+        }
+        const mouseup = () => {
+            document.removeEventListener('mousemove', drag);
+            document.removeEventListener('mouseup', mouseup);
+        }
+
+        document.addEventListener('mouseup', mouseup);
+        document.addEventListener('mousemove', drag);
+
     });
  }
 
@@ -56,15 +161,20 @@ class CodePreview extends HTMLElement {
 
     // Set up the HTML content for the shadow DOM
     container.innerHTML = `
-      <div id="header" style="cursor: move;">
-        <div style="color: white; background: black;">Preview</div>
+      <div class="header" style="cursor: move; color: white; background: black;">
+        <div>Preview</div>
       </div>
-      <div id="content">
-        <div id="iframe-container">
+      <div id="content" style="height: calc(100% - 24px)">
+        <div id="iframe-container" style="height: 100%">
           <!-- You can replace 'your-url-here' with the desired URL -->
           <iframe src=${iframe_url} frameborder="0" width="100%" height="100%"></iframe>
         </div>
       </div>
+      <div class="corner-rb" style="position: absolute; bottom: 0; right: 0; width: 12px; height: 12px; cursor: nwse-resize"></div>
+      <div class="corner-lb" style="position: absolute; bottom: 0; left: 0; width: 12px; height: 12px; cursor: nesw-resize"></div>
+      <div class="corner-l" style="position: absolute; left: 0; top: 0; width: 12px; height: calc(100% - 12px); cursor: ew-resize"></div>
+      <div class="corner-r" style="position: absolute; right: 0; top: 0; width: 12px; height: calc(100% - 12px); cursor: ew-resize"></div>
+      <div class="corner-b" style="position: absolute; left: 12px; bottom: 0; width: calc(100% - 24px); height: 12px; cursor: ns-resize"></div>
     `;
 
     dragElement(container);
@@ -75,4 +185,3 @@ class CodePreview extends HTMLElement {
 }
 
 customElements.define('code-preview', CodePreview);
-
