49590beb8b96682ff23f3627718a2a40ca121ae2
Arpita-Jaiswal <ayushipujaa@gmail.com>
2024-02-29 15:37:20 +0530

    Editor Preview UI fixes

--- web-components/code-preview.js
+++ web-components/code-preview.js
@@ -1,4 +1,4 @@
-function dragElement(element) {
+function dragElement(element, dragBG) {
     // Reference to the entire window
     const windowElement = element;
     const title= element.querySelector('.header');
@@ -12,7 +12,10 @@ function dragElement(element) {
     let expand = true
 
     title.addEventListener('mousedown', (e) => {
-        title.style.background = '#202020';
+        let oldBackground = title.style.background;
+        let oldBorder = content.style.borderColor;
+        title.style.background = dragBG.color;
+        content.style.borderColor = dragBG.color;
         // Initialize variables for tracking drag state
         let offsetX = e.clientX - windowElement.getBoundingClientRect().left;
         let offsetY = e.clientY - windowElement.getBoundingClientRect().top;
@@ -27,7 +30,8 @@ function dragElement(element) {
             windowElement.style.top = `${newY}px`;
         }
         const mouseup = () => {
-            title.style.background = 'black';
+            title.style.background = oldBackground;
+            content.style.borderColor = oldBorder;
 
             document.removeEventListener('mousemove', drag);
             document.removeEventListener('mouseup', mouseup);
@@ -168,8 +172,9 @@ class CodePreview extends HTMLElement {
     container.style.width = '400px';
     container.style.height = '400px';
     container.style.zIndex = '1000';
-
+    let data = window.ftd.component_data(this);
     var iframe_url = "https://fastn.com";
+    let dragBG = {color: '#636161'};
 
     // Set up the HTML content for the shadow DOM
     container.innerHTML = `
@@ -177,7 +182,7 @@ class CodePreview extends HTMLElement {
         <div>Preview</div>
         <div class="minimize" style="cursor: pointer">--</div>
       </div>
-      <div class="content" style="height: calc(100% - 24px)">
+      <div class="content" style="height: calc(100% - 24px); border-width: 1px; border-style: solid; border-color: black">
         <div id="iframe-container" style="height: 100%">
           <!-- You can replace 'your-url-here' with the desired URL -->
           <iframe src=${iframe_url} frameborder="0" width="100%" height="100%"></iframe>
@@ -190,7 +195,27 @@ class CodePreview extends HTMLElement {
       <div class="corner-b" style="position: absolute; left: 12px; bottom: 0; width: calc(100% - 24px); height: 12px; cursor: ns-resize"></div>
     `;
 
-    dragElement(container);
+      const title= container.querySelector('.header');
+      const content= container.querySelector('.content');
+      let headerColor = function () {
+          const darkMode = data.dark_mode.get();
+          if (darkMode) {
+              title.style.backgroundColor = 'white';
+              title.style.color = 'black';
+              content.style.borderColor = 'white';
+              dragBG.color = '#c5c2c2';
+          } else {
+              title.style.backgroundColor = 'black';
+              title.style.color = 'white';
+              content.style.borderColor = 'black';
+              dragBG.color = '#636161';
+          }
+      };
+      data.dark_mode.on_change(headerColor);
+
+
+      headerColor();
+      dragElement(container, dragBG);
 
     // Append the container to the shadow DOM
     this.shadowRoot.appendChild(container);
