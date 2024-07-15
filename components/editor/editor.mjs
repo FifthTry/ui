910614f055a7eb9befe02c5cd6ba2f4063ca9b21
siddhantCodes <siddhantk232@gmail.com>
2024-07-15 15:24:56 +0530

    fix: only debounce the `save-unsaved-file` event

--- components/editor/editor.mjs
+++ components/editor/editor.mjs
@@ -33,14 +33,12 @@ class CMEditor extends HTMLElement {
          */
         function update(vu) {
             self.documents[self.currentDocument] = vu.state;
-            if (vu.docChanged) {
-                window.ide_dispatch_event("save-unsaved-file", {file_name: self.currentDocument, content: vu.state.doc.toString()});
-            }
+            syncToWorkspace(self.currentDocument, vu.state.doc.toString());
         }
 
         function get_extensions(language, update) {
             // TODO: move this debounce inside rust wasm
-            let extensions = [basicSetup, javascript(), EditorView.updateListener.of(debounce(update, 200))];
+            let extensions = [basicSetup, javascript(), EditorView.updateListener.of(update)];
             switch (language) {
                 case 'Python':
                     extensions.push(python());
@@ -77,6 +75,11 @@ class CMEditor extends HTMLElement {
             });
             window.ide_cm_editor.setState(this.documents[this.currentDocument]);
         });
+
+
+        const syncToWorkspace = debounce((file_name, content) => {
+            window.ide_dispatch_event("save-unsaved-file", { file_name, content });
+        }, 600);
     }
 }
 
