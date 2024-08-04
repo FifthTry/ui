23c1619e30243ef4ad7d5e283b36df5b0ead3ec2
Amit Upadhyay <upadhyay@gmail.com>
2024-08-04 19:05:04 +0530

    some syntax highlighting

--- components/editor/command-k/language.js
+++ components/editor/command-k/language.js
@@ -1,27 +1,19 @@
 import {parser} from "./command.grammar"
-import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import {LRLanguage, LanguageSupport} from "@codemirror/language"
 import {styleTags, tags as t} from "@lezer/highlight"
 
 export const EXAMPLELanguage = LRLanguage.define({
     parser: parser.configure({
         props: [
-            indentNodeProp.add({
-                Application: delimitedIndent({closing: ")", align: false})
-            }),
-            foldNodeProp.add({
-                Application: foldInside
-            }),
             styleTags({
-                Identifier: t.variableName,
-                Boolean: t.bool,
-                String: t.string,
+                Identifier: t.literal,
+                AddFile: t.keyword,
                 LineComment: t.lineComment,
-                "( )": t.paren
             })
         ]
     }),
     languageData: {
-        commentTokens: {line: ";"}
+        commentTokens: {line: "-- "}
     }
 })
 
