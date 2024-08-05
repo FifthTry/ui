70c61133fa59de2372f296d3991a574ea1fd84c6
Amit Upadhyay <upadhyay@gmail.com>
2024-08-05 11:55:40 +0530

    better command k keyboard handling

--- components/editor/command-k/command-editor.mjs
+++ components/editor/command-k/command-editor.mjs
@@ -8,7 +8,7 @@ export class CommandEditor extends HTMLElement {
         super();
         this.style.width = "100%";
         this.style.height = "100%";
-        console.log("CommandEditor constructor")
+        console.log("CommandEditor constructor11")
     }
 
     connectedCallback() {
@@ -22,15 +22,6 @@ export class CommandEditor extends HTMLElement {
     }
 }
 
-function escapeKey() {
-    return {
-        key: "Escape", run() {
-            ftd.set_value("ui.fifthtry.com/components/editor/vars#command-k", false);
-            return false;
-        }
-    };
-}
-
 function enterIs(keys) {
     return keys.map((key) => {
         return keymap.of([{key, run: insertNewlineAndIndent}]);
@@ -38,13 +29,20 @@ function enterIs(keys) {
 }
 
 window.ide_open_command_k = (cmd) => {
+    if (cmd.length) {
+        // we intentionally add a space at the end so that the user can start typing
+        cmd = cmd + " ";
+    }
+
     ftd.set_value("ui.fifthtry.com/components/editor/vars#command-k", true);
     window.command_editor.dispatch({
         changes: {
             from: 0,
             to: window.command_editor.state.doc.length,
             insert: cmd
-        }
+        },
+        // move the focus to end of text as that is where you usually want to begin typing
+        selection: {anchor: cmd.length, head: cmd.length}
     });
     window.command_editor.focus();
 }
@@ -53,11 +51,11 @@ function get_extensions() {
     return [
         enterIs(["Alt-Enter", "Cmd-Enter", "Ctrl-Enter", "Shift-Enter"]),
         keymap.of([
-            escapeKey(),
             {
                 key: "Enter",
                 run() {
                     console.log("enter pressed");
+                    // instead of parsing can we get it from the editor state?
                     let tree = p.parse(window.command_editor.state.doc.toString());
                     if (!!window.ide_parse_command) {
                         window.ide_parse_command(window.command_editor.state.doc, tree);
@@ -76,3 +74,15 @@ function get_extensions() {
         example(),
     ];
 }
+
+document.addEventListener("keydown", (e) => {
+    console.log(e, e.key, e.metaKey, e.ctrlKey);
+    if (e.key === "Escape") {
+        ftd.set_value("ui.fifthtry.com/components/editor/vars#command-k", false);
+    }
+    if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        console.log("command-k pressed");
+        e.preventDefault();
+        ftd.set_value("ui.fifthtry.com/components/editor/vars#command-k", true);
+    }
+});
